d3eb1acb4a48444bffb1d5ea3c6ce32b97d897a9
Yali07 <yaliprogrammer07@gmail.com>
2020-09-13 21:50:14 +0530

    modify media root

--- tpt/settings.py
+++ tpt/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
