2ed2c9a70582b25474541a1a0f0ab13593597359
Yali07 <yaliprogrammer07@gmail.com>
2020-09-15 19:12:03 +0530

    modify snippet image path

--- blog/models.py
+++ blog/models.py
@@ -17,13 +17,14 @@ class BlogPost(models.Model):
     def __str__(self):
     	return self.title
     def save(self, *args,**kwargs):
+        super().save(*args,**kwargs)
         self.slug = slugify(self.title)
         img = Image.open(self.snippet_image.path)
         if img.height > 250 or img.width > 250:
             output_size = (250,250)
             img.thumbnail(output_size)
             img.save(self.snippet_image.path)
-        super().save(*args,**kwargs)
+        
         
     def get_absolute_url(self):
     	return f"/{self.slug}"      
