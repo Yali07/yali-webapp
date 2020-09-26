01188825f7365b2f6dd207e4fb3a919da2e45d0b
Yali07 <68160587+Yali07@users.noreply.github.com>
2020-09-26 22:44:28 +0530

    Update settings.py

--- tpt/settings.py
+++ tpt/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x3=#u-_bk$s6qjxwrw$6yk8ut9u47f(z7mxp)t#n%$g7i!@_#d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.getcwd() == "apps/":
-    Debug = False
-else:
-    Debug = True
+Debug = True
+if os.getcwd() == "/app" :
+	Debug = False
 ALLOWED_HOSTS = ['yaliprogramming.herokuapp.com','127.0.0.1']
 
 
