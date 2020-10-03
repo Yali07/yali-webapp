36ccd6a4b501db32509ffd980fb4edf112654423
Yali07 <68160587+Yali07@users.noreply.github.com>
2020-10-03 19:24:48 +0530

    Update settings.py

--- tpt/settings.py
+++ tpt/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = 'x3=#u-_bk$s6qjxwrw$6yk8ut9u47f(z7mxp)t#n%$g7i!@_#d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
+DEBUG_PROPAGATE_EXCEPTIONS = True
 ALLOWED_HOSTS = ['yaliprogramming.herokuapp.com','127.0.0.1']
 
 
@@ -124,7 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
-
+COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_SSL = True
